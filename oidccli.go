7b4c28fefdd376331e9c0a9367811002ceb0bc70
Alex Polvi <alex@polvi.net>
2015-10-12 13:01:30 -0700

    add once method

--- oidccli.go
+++ oidccli.go
@@ -11,7 +11,36 @@ import (
 	"time"
 )
 
-func oidcCallback(c *oidc.Client, listenAddr string) (string, chan *oauth2.TokenResponse, error) {
+func oidcCallback(c *oidc.Client, listenAddr string) (string, chan jose.JWT, error) {
+	tokenChan := make(chan jose.JWT)
+	l, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return "", nil, err
+	}
+	oac, err := c.OAuthClient()
+	if err != nil {
+		return "", nil, err
+	}
+	f := func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			return
+		}
+		token, err := c.ExchangeAuthCode(code)
+		if err != nil {
+			fmt.Fprintf(w, "error: %s", err)
+			return
+		}
+		tokenChan <- token
+		close(tokenChan)
+		fmt.Fprintf(w, "Success! You can now close this window and go back to the CLI")
+		l.Close()
+	}
+	go http.Serve(l, http.HandlerFunc(f))
+	return oac.AuthCodeURL("", "", ""), tokenChan, err
+}
+
+func oauth2Callback(c *oidc.Client, listenAddr string) (string, chan *oauth2.TokenResponse, error) {
 	tokenChan := make(chan *oauth2.TokenResponse)
 	l, err := net.Listen("tcp", listenAddr)
 	if err != nil {
@@ -38,6 +67,17 @@ func oidcCallback(c *oidc.Client, listenAddr string) (string, chan *oauth2.Token
 	go http.Serve(l, http.HandlerFunc(f))
 	return oac.AuthCodeURL("", "", ""), tokenChan, err
 }
+func IdentityOnce(clientID, clientSecret, discoveryURL, redirectURL string) (string, chan jose.JWT, error) {
+	oic, err := GetOIDCClient(clientID, clientSecret, discoveryURL, redirectURL)
+	if err != nil {
+		return "", nil, err
+	}
+	u, err := url.Parse(redirectURL)
+	if err != nil {
+		return "", nil, err
+	}
+	return oidcCallback(oic, u.Host)
+}
 
 // Grabs a refresh token and then continues to use it to refresh identities,
 // returning the resulting JWT over the chan. Returns the AuthCode URL and chan of JWTs
@@ -51,7 +91,7 @@ func RefreshingIdentity(clientID, clientSecret, discoveryURL, redirectURL string
 	if err != nil {
 		return "", nil, err
 	}
-	authURL, tokenChan, err := oidcCallback(oic, u.Host)
+	authURL, tokenChan, err := oauth2Callback(oic, u.Host)
 	if err != nil {
 		return "", nil, err
 	}
@@ -101,6 +141,7 @@ func GetOIDCClient(clientID, clientSecret, discoveryURL, redirectURL string) (*o
 		ProviderConfig: cfg,
 		Credentials:    cc,
 		RedirectURL:    redirectURL,
+		Scope:          []string{"offline_access", "openid", "email", "profile"},
 	}
 	oidcClient, err := oidc.NewClient(ccfg)
 	if err != nil {
